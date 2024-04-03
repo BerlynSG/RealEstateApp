4b32d85052d58c336efb5fb0f74ffef4e7bf7754
BerlynSG <135674997+BerlynSG@users.noreply.github.com>
2024-04-02 21:45:56 -0400

    Añadiendo algunas características de lista de propiedades a la lista de agentes

--- RealEstateApp/Controllers/AgenteController.cs
+++ RealEstateApp/Controllers/AgenteController.cs
@@ -22,21 +22,21 @@ namespace RealEstateApp.Controllers
         {
             _agentes = new List<AgenteViewModel>
             {
-                new AgenteViewModel { Id = 1, Nombre = "Juan", Apellidos = "Perez", Foto = "juan.jpg", Propiedades = new List<PropiedadViewModel>
+                new AgenteViewModel { Id = 1, Nombre = "Juan", Apellidos = "Perez", Foto = "/img/Agentes/Agente.jpeg", Propiedades = new List<PropiedadViewModel>
                     {
-                        new PropiedadViewModel { Codigo = "001", TipoPropiedad = tiposPropiedad[2], TipoVenta = tiposVenta[2], Valor = 150000, Habitaciones = 3, Baños = 2, Tamaño = 200, Descripcion = "Bonita casa en zona residencial", Imagenes = new List<string> { "casa1.jpg", "casa2.jpg", "casa3.jpg" } },
-                        new PropiedadViewModel { Codigo = "002", TipoPropiedad = tiposPropiedad[1], TipoVenta = tiposVenta[1], Valor = 1000, Habitaciones = 2, Baños = 1, Tamaño = 100, Descripcion = "Acogedor apartamento amueblado", Imagenes = new List<string> { "apto1.jpg", "apto2.jpg" } }
+                        new PropiedadViewModel { Codigo = "001", TipoPropiedad = tiposPropiedad[2], TipoVenta = tiposVenta[2], Valor = 150000, Habitaciones = 3, Baños = 2, Tamaño = 200, Descripcion = "Bonita casa en zona residencial", Imagenes = new List<string> { "/img/Propiedades/Apartamento.jpg", "/img/Propiedades/Apartamento.jpg", "/img/Propiedades/Apartamento.jpg" } },
+                        new PropiedadViewModel { Codigo = "002", TipoPropiedad = tiposPropiedad[1], TipoVenta = tiposVenta[1], Valor = 1000, Habitaciones = 2, Baños = 1, Tamaño = 100, Descripcion = "Acogedor apartamento amueblado", Imagenes = new List<string> { "/img/Propiedades/Apartamento.jpg", "/img/Propiedades/Apartamento.jpg" } }
                     }
                 },
-                new AgenteViewModel { Id = 2, Nombre = "Maria", Apellidos = "Gonzalez", Foto = "maria.jpg", Propiedades = new List<PropiedadViewModel>
+                new AgenteViewModel { Id = 2, Nombre = "Maria", Apellidos = "Gonzalez", Foto = "/img/Agentes/Agente.jpeg", Propiedades = new List<PropiedadViewModel>
                     {
-                        new PropiedadViewModel { Codigo = "003", TipoPropiedad = tiposPropiedad[2], TipoVenta = tiposVenta[2], Valor = 200000, Habitaciones = 4, Baños = 3, Tamaño = 250, Descripcion = "Amplia casa con jardín", Imagenes = new List<string> { "casa4.jpg", "casa5.jpg", "casa6.jpg" } }
+                        new PropiedadViewModel { Codigo = "003", TipoPropiedad = tiposPropiedad[2], TipoVenta = tiposVenta[2], Valor = 200000, Habitaciones = 4, Baños = 3, Tamaño = 250, Descripcion = "Amplia casa con jardín", Imagenes = new List<string> { "/img/Propiedades/Apartamento.jpg", "/img/Propiedades/Apartamento.jpg", "/img/Propiedades/Apartamento.jpg" } }
                     }
                 },
-                new AgenteViewModel { Id = 3, Nombre = "Carlos", Apellidos = "Lopez", Foto = "carlos.jpg", Propiedades = new List<PropiedadViewModel>
+                new AgenteViewModel { Id = 3, Nombre = "Carlos", Apellidos = "Lopez", Foto = "/img/Agentes/Agente.jpeg", Propiedades = new List<PropiedadViewModel>
                     {
-                        new PropiedadViewModel { Codigo = "004", TipoPropiedad = tiposPropiedad[2], TipoVenta = tiposVenta[2], Valor = 80000, Habitaciones = 1, Baños = 1, Tamaño = 80, Descripcion = "Moderno apartamento con vista al mar", Imagenes = new List<string> { "apto3.jpg", "apto4.jpg" } },
-                        new PropiedadViewModel { Codigo = "005", TipoPropiedad = tiposPropiedad[3], TipoVenta = tiposVenta[2], Valor = 50000, Tamaño = 500, Descripcion = "Terreno en urbanización cerrada", Imagenes = new List<string> { "terreno1.jpg" } }
+                        new PropiedadViewModel { Codigo = "004", TipoPropiedad = tiposPropiedad[2], TipoVenta = tiposVenta[2], Valor = 80000, Habitaciones = 1, Baños = 1, Tamaño = 80, Descripcion = "Moderno apartamento con vista al mar", Imagenes = new List<string> { "/img/Propiedades/Apartamento.jpg", "/img/Propiedades/Apartamento.jpg" } },
+                        new PropiedadViewModel { Codigo = "005", TipoPropiedad = tiposPropiedad[3], TipoVenta = tiposVenta[2], Valor = 50000, Tamaño = 500, Descripcion = "Terreno en urbanización cerrada", Imagenes = new List<string> { "/img/Propiedades/Apartamento.jpg" } }
                     }
                 }
             };
