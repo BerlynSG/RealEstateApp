112ae65a82666c1ccbbcf783590e7f33bcdc073f
BerlynSG <135674997+BerlynSG@users.noreply.github.com>
2024-04-18 11:26:16 -0400

    Arreglando algunas cosas

--- RealStateApp.Infrastructure.Identity/Services/AccountService.cs
+++ RealStateApp.Infrastructure.Identity/Services/AccountService.cs
@@ -502,7 +502,7 @@ namespace RealEstateApp.Infrastructure.Identity.Services
                     Rol = user.Rol,
                     ImagePath = user.ImagePath,
                     Phone = user.PhoneNumber,
-                    Roles = rol.ToList(),
+                    Roles = rol.ToList()
                 };
 
                 response.Add(user_res);
