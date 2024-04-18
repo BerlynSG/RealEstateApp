af7ab58f5a1b2a216a96d9b4db64973fafef6829
BerlynSG <135674997+BerlynSG@users.noreply.github.com>
2024-04-18 17:25:16 -0400

    Arreglando la falta de información al hacer un getAll de propiedades en el api

--- RealEstateApp.Core.Application/Features/Propiedades/Queries/GetAllPropiedades/GetAllPropiedadesQuery.cs
+++ RealEstateApp.Core.Application/Features/Propiedades/Queries/GetAllPropiedades/GetAllPropiedadesQuery.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using MediatR;
 using RealEstateApp.Core.Application.Interfaces.Repositories;
+using RealEstateApp.Core.Application.Interfaces.Services;
+using RealEstateApp.Core.Application.ViewModels.Agente;
 using RealEstateApp.Core.Application.ViewModels.Propiedad;
 
 namespace RealEstateApp.Core.Application.Features.Propiedades.Queries.GetAllPropiedades
@@ -16,11 +18,13 @@ namespace RealEstateApp.Core.Application.Features.Propiedades.Queries.GetAllProp
     public class GetAllPropiedadesQueryHandler : IRequestHandler<GetAllPropiedadesQuery, IList<PropiedadViewModel>>
     {
         private readonly IPropiedadRepository _propiedadRepository;
+        private readonly IUserService _userService;
         private readonly IMapper _mapper;
 
-        public GetAllPropiedadesQueryHandler(IPropiedadRepository propiedadRepository, IMapper mapper)
+        public GetAllPropiedadesQueryHandler(IPropiedadRepository propiedadRepository, IUserService userService, IMapper mapper)
         {
             _propiedadRepository = propiedadRepository;
+            _userService = userService;
             _mapper = mapper;
         }
 
@@ -33,9 +37,12 @@ namespace RealEstateApp.Core.Application.Features.Propiedades.Queries.GetAllProp
 
         private async Task<List<PropiedadViewModel>> GetAllViewModel()
         {
-            var entityList = await _propiedadRepository.GetAllAsync();
+            var entityList = await _propiedadRepository.GetAllWithInclude();
+            var agentes = await _userService.GetAllUsers();
+            var propiedades = _mapper.Map<List<PropiedadViewModel>>(entityList);
+            propiedades.ForEach(p => p.Agente = _mapper.Map<AgenteViewModel>(agentes.FirstOrDefault(a => a.Id == p.AgenteId)));
 
-            return _mapper.Map<List<PropiedadViewModel>>(entityList);
+            return propiedades;
         }
     }
 }
